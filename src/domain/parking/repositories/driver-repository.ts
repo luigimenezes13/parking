63285918dc6858caa0fbcacaaccd4f184c17ece6
LuigiMenezes1311 <luigi.menezes@v4company.com>
2026-04-24 16:28:12 -0300

    feat: add repository interfaces for Driver, ParkingLot, ParkingSpot, and Vehicle to manage domain entities

---
+++ src/domain/parking/repositories/driver-repository.ts
@@ -0,0 +1,9 @@
+import { type UniqueIdentifier } from '@domain/shared/value-objects/unique-identifier.ts';
+import { type Driver } from '@domain/parking/entities/driver.ts';
+
+export interface DriverRepository {
+  save(driver: Driver): Promise<void>;
+  findById(identifier: UniqueIdentifier): Promise<Driver | null>;
+  findByCnh(cnh: string): Promise<Driver | null>;
+  findByEmail(email: string): Promise<Driver | null>;
+}
